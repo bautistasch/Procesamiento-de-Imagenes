38ea3516123520c283c40cf8204483e3ac7282fe
santiagomangone <smangone@itba.edu.ar>
2023-11-22 17:14:54 -0300

    Push Implementation

--- TP8/implementation.py
+++ TP8/implementation.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 from property import Range
-
+from scipy.interpolate import RectBivariateSpline
+from skimage.data import shepp_logan_phantom
+from skimage.transform import radon, rescale, rotate
+from skimage.transform import radon, iradon
 
 def radon(image: np.ndarray, angles: list[float]) -> list[np.ndarray]:
-    # TODO: Hacer
-    return [image for angle in angles]
+    sinogram = radon(image, theta=angles)
+    return sinogram
+
 
 def inverse_radon(radon: list[tuple[float, np.ndarray]], interpolation: str, filter: str, size: int) -> np.ndarray:
-    # TODO: Hacer
+    reconstruction_img = iradon(radon[1], theta=radon[0], filter_name='ramp')
     res = np.zeros((size, size), dtype=np.float32)
-    return res
+    return reconstruction_img
